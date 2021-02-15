0b86f067c4c6911b9b6f7e80894d9fffc0b60de1
JaehyeonHeo <jhheo06@gmail.com>
2021-02-15 15:13:25 +0900

    210215 10장 11장 커밋

---
+++ 10_transaction/transaction_trycatch.sql
@@ -0,0 +1,37 @@
+﻿use sqlDB;
+
+/*create table bankBook (
+	uName nvarchar(10),
+	umoney int, 
+		constraint CK_money
+		check (uMoney >= 0) 
+);
+go 
+insert into bankBook values (N'구매자', 1000); 
+insert into	bankBook values (N'판매자', 0); 
+*/
+
+begin try
+	begin tran
+	update bankBook set uMoney = umoney - 500 where uname = N'구매자'; 
+	update bankBook set uMoney = umoney + 500 where uname = N'판매자'; 
+	commit tran
+end try 
+begin catch 
+	rollback
+end catch 
+	select * from bankBook; 
+
+begin try 
+	begin tran
+	update bankBook set umoney = umoney - 600 where uName = N'구매자';
+	update bankBook set umoney = umoney + 600 where uName = N'판매자'; 
+	commit tran
+end try
+
+
+begin catch 
+	rollback 
+end catch
+
+select * from bankBook; 
