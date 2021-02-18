a23914bdfda4552a32feb4c4a114a8237823205b
JaehyeonHeo <jhheo06@gmail.com>
2021-02-18 12:31:37 +0900

    210218 16장 실습용 쿼리 커밋

---
+++ 16_practice/2100218_insertData쿼리.sql
@@ -0,0 +1,58 @@
+﻿-- 데이터 입력 
+insert into booksTBL 
+(
+	cateidx
+  , bookName
+  , author
+  , company
+  , releaseDate
+  , ISBN
+  , price
+) 
+values 
+(
+	    'N0002'
+	  , '순수하지 않은 감각'
+	  , '요안나'
+	  , '로코코'
+	  , '2019-10-02'
+	  , '9791135445705'
+	  , 12500
+); 
+
+--책테이블 위 책정보 중, description, regDate 정보 수정 (update 사용시에는 where절 매우 중요!!!!)
+			-- 비긴트랜을 사용하면 롤백을 통해 데이터를 다시 되돌릴 수 있다.
+			-- 비긴트랜 사용시 반드시 롤백 또는 커밋을 해야 종료됨 (안하면 데이터수정불가)
+begin tran; 
+
+update booksTBL 
+   set descriptions = '부명그룹의 브레인이자 핵심 TF팀 리더, 강태욱 수석.'
+     , regDate = '2021-02-18' 
+ where bookidx = 13
+
+
+ rollback;
+ commit;
+
+ select * from booksTBL; 
+
+
+ -- 카테고리 입력 (insert into)
+ begin tran; 
+
+ insert into cateTBL /*(cateidx, cateName)*/ 
+	values ('I0001', '대화/협상')
+		 , ('I0002', '성공/처세')					
+	     , ('I0003', '시간관리')
+		 , ('I0004', '인간관계'); 
+
+rollback; 
+commit; 
+
+select * from cateTBL ; 
+
+
+-- 카테고리 삭제 (delete from)
+delete from cateTBL where cateidx  = 'I0003'; 
+
+delete from cateTBL where cateidx in ('I0001', 'I0002'); 
---
+++ 16_practice/210218_uv_view.sql
@@ -0,0 +1,21 @@
+﻿create view uv_nonerentalMember 
+as
+     select r.rentalidx
+        --, r.memberidx
+	      , m.memberName
+		--, r.bookidx
+		  , b.cateidx
+		  , c.cateName
+		  , b.bookName
+		  , b.author
+		  , format(r.rentalDt, 'yyyy년 MM월 dd일') as 'rentalDate'
+		  , isnull(format(r.rerurnDt, 'yyyy년 MM월 dd일'), '미정') as 'returnDate'
+		  , dbo.ufn_getState(r.rentalState) as '대여상태' --사용자 지정 함수 
+  from dbo.rentalTBL as r
+ right outer join memberTBL as m 
+    on r.memberidx = m.memberidx
+ left outer join booksTBL as b 
+    on r.bookIdx = b.bookIdx
+ left outer join cateTBL as c 
+    on c.cateidx = b.cateidx
+ where r.rentalidx is null;
